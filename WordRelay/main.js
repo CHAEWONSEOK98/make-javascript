fb0fce9a7ca3a57d332dbbcbdfd2aaa95b1b372f
CHAEWONSEOK98 <83595773+CHAEWONSEOK98@users.noreply.github.com>
2022-01-14 11:49:22 +0900

    wordrelay update

--- WordRelay/main.js
+++ WordRelay/main.js
@@ -6,8 +6,25 @@ const btn = document.querySelector("#btn");
 const arr = new Array();
 
 btn.addEventListener("click", (event) => {
-  arr[0] = input.value;
-  console.log(arr[0]);
+  arr[0] === undefined ? (arr[0] = input.value) : (arr[1] = input.value);
+  const brr0 = arr[0].split("");
+  const p1 = document.createElement("p");
+  if (arr[1] === undefined) {
+    p1.innerText = `${arr[0]}`;
+    container.appendChild(p1);
+    input.value = "";
+  } else {
+    const brr1 = arr[1].split("");
+    if (brr0[brr0.length - 1] === brr1[0]) {
+      p1.innerText = `${arr[1]}`;
+      container.appendChild(p1);
+      input.value = "";
+      arr[0] = arr[1];
+    } else {
+      p1.innerText = `땡!`;
+      container.appendChild(p1);
+    }
+  }
 });
 
 form.addEventListener("submit", handleSubmit);
