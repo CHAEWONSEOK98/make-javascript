3b42896a7155c0d2710d65c9dfdad6ef6a9d5067
CHAEWONSEOK98 <83595773+CHAEWONSEOK98@users.noreply.github.com>
2022-01-18 22:51:42 +0900

    getRandomNumber.js

--- RandomBall/main.js
+++ RandomBall/main.js
@@ -1,19 +1,22 @@
 const $btn = document.querySelector("#btn");
 const $showRandomBall = document.querySelector("#showRandomBall");
 
-const saveBall = [];
-saveBall.length = 45;
-const saveBallStore = saveBall.fill(0).map((item, idx) => idx + 1);
+function getRandomNumber() {
+  const saveBall = [];
+  saveBall.length = 45;
+  const saveBallStore = saveBall.fill(0).map((item, idx) => idx + 1);
 
-const index = Math.floor(Math.random() * 45 + 1);
+  let randomBall = [];
 
-const randomBall = [];
-randomBall.length = 6;
-const randomBallStore = randomBall
-  .fill(0)
-  .map((item, idx) => idx + saveBallStore[index]);
+  for (let i = 0; i < 6; i++) {
+    const index = Math.floor(Math.random() * 45); //0~44
+    randomBall.push(saveBallStore[index]);
+    saveBallStore.splice(index, 1);
+  }
+
+  $showRandomBall.textContent = `${randomBall}`;
+}
 
 $btn.addEventListener("click", () => {
-  $showRandomBall.textContent = `${randomBallStore}`;
-  console.log(`${randomBallStore}`);
+  getRandomNumber();
 });
